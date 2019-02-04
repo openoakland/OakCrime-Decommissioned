b52556192e3c8e2aab60970f4e23cbe481c8ed6e
Aaron D Borden <adborden@a14n.net>
2019-02-03 22:21:28 -0800

    [terraform] fix beanstalk app lifecycle
    Fixes:
    
    > * aws_elastic_beanstalk_application.default: InsufficientPrivilegesException: Insufficient privileges for IAM PassRole Action.
    >   status code: 403

--- terraform/beanstalk_app/main.tf
+++ terraform/beanstalk_app/main.tf
@@ -1,3 +1,7 @@
+data "aws_iam_role" "beanstalk_service" {
+  name = "aws-elasticbeanstalk-service-role"
+}
+
 resource "aws_route53_zone" "default" {
   name = "${var.dns_zone}"
 }
@@ -7,7 +11,7 @@ resource "aws_elastic_beanstalk_application" "default" {
 
   // Retain the recent application versions/deploys
   appversion_lifecycle {
-    service_role          = "aws-elasticbeanstalk-service-role"
+    service_role          = "${data.aws_iam_role.beanstalk_service.arn}"
     max_count             = "${var.appversion_lifecycle_max_count}"
     delete_source_from_s3 = "${var.delete_source_from_s3}"
   }
