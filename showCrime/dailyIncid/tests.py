de44ca5a3d879300e1ea6f21db2ad8bf04c1503b
Aaron D Borden <adborden@a14n.net>
2019-04-16 20:30:40 -0700

    Tests
    Any tests are better than none.

--- showCrime/dailyIncid/tests.py
+++ showCrime/dailyIncid/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import OakCrime
+from .management.commands import harvestSocrata
+
+class TestDailyIncid(TestCase):
+
+    def test_model_oakcrime(self):
+        # This isn't much of a test since the import worked
+        assert OakCrime
+
+    def test_harvest_socrata_cleanOPDtext(self):
+        assert harvestSocrata.cleanOPDtext(' input ') == 'input', 'white space should be striped'
+        assert harvestSocrata.cleanOPDtext('one two') == 'one_two', 'spaces should be converted'
